1f7b70e9eb93e3b9de0916ca0a6b10f44ba65e17
Viet Tran <26016342+DamSenViet@users.noreply.github.com>
2018-02-03 20:52:15 -0800

    UI

---
+++ docs_copy/main.js
@@ -0,0 +1,22 @@
+
+
+// this needs to be at the end
+$(".img-camera").click(function() {
+    $("#file-select").click();
+
+    var img = document.querySelector(".img-camera");
+    var fileSelect = document.getElementById('file-select');
+    var file = fileSelect.files[0];
+
+
+    var reader = new FileReader();
+    reader.addEventListener("load", function() {
+        img.src = reader.result;
+        img.style.width = "auto";
+        img.style.border = "0px";
+    });
+
+    if (file) {
+        reader.readAsDataURL(file);
+    }
+});
